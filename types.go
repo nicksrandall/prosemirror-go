2915e0abf681a6c82e7ac227fe986de214497e84
Nick Randall <nick.randall@domo.com>
2017-05-11 16:19:23 -0600

    adding more docs to types

--- types.go
+++ types.go
@@ -1,10 +1,12 @@
 package prosemirror
 
+// EditorState - http://prosemirror.net/docs/ref/#state.Editor_State
 type EditorState struct {
 	Doc       *Content   `json:"doc"`
 	Selection *Selection `json:"selection"`
 }
 
+// Content - http://prosemirror.net/docs/ref/#model.Node
 type Content struct {
 	Attrs   map[string]interface{} `json:"attrs,omitempty"`
 	Content []*Content             `json:"content,omitempty"`
@@ -13,12 +15,14 @@ type Content struct {
 	Type    string                 `json:"type"`
 }
 
+// Selection - http://prosemirror.net/docs/ref/#state.Selection
 type Selection struct {
 	Anchor int    `json:"anchor"`
 	Head   int    `json:"head"`
 	Type   string `json:"type"`
 }
 
+// Mark - http://prosemirror.net/docs/ref/#model.Mark
 type Mark struct {
 	Attrs map[string]interface{} `json:"attrs,omitempty"`
 	Type  string                 `json:"type"`
